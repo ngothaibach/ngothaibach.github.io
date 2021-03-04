a87e813bbddc69aac45e35b06809caba0259a314
TaiPV <tai97nd@gmail.com>
2021-03-04 12:24:42 +0700

    Update index.blade.php

--- packages/Webkul/Exchange/src/Resources/views/admin/purchase/index.blade.php
+++ packages/Webkul/Exchange/src/Resources/views/admin/purchase/index.blade.php
@@ -237,8 +237,8 @@
                             console.error('loix ne ', response);
                             if (response.data.success == true) {
                                 console.error("save exchange successfull");
-                                // window.location.href =
-                                //     "{{ route('admin.exchange.purchase-order.list') }}";
+                                window.location.href =
+                                    "{{ route('admin.exchange.purchase-order.list') }}";
                             } else {
                                 console.debug("save exchange NOT successfull");
                             }
