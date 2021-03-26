4049fddccb9686eb4115815fdf9fc0ef4a9740b0
ngothaibach <77270269+ngothaibach@users.noreply.github.com>
2021-03-26 09:03:47 +0700

    improve_add_product

--- packages/Webkul/Admin/src/Resources/views/catalog/products/field-types/text.blade.php
+++ packages/Webkul/Admin/src/Resources/views/catalog/products/field-types/text.blade.php
@@ -1,4 +1,4 @@
-<input type="text" v-validate="'{{$validations}}'" class="control" id="{{ $attribute->code }}" name="{{ $attribute->code }}" @isset($product) value="{{ old($attribute->code) ?: $product[$attribute->code] }}" @endisset {{ in_array($attribute->code, ['sku', 'url_key']) ? 'v-slugify' : '' }} data-vv-as="&quot;{{ $attribute->admin_name }}&quot;" @if(isset($product)) {{ $attribute->code == 'name' && ! $product[$attribute->code]  ? 'v-slugify-target=\'url_key\'' : ''  }} @endif />
+<input type="text" v-validate="'{{$validations}}'" class="control" id="{{ $attribute->code }}" name="{{ $attribute->code }}" @if(isset($product)) value="{{ old($attribute->code) ?: $product[$attribute->code] }}" @else value="{{ old($attribute->code) }}" @endif  {{ in_array($attribute->code, ['sku', 'url_key']) ? 'v-slugify' : '' }} data-vv-as="&quot;{{ $attribute->admin_name }}&quot;" @if(isset($product)) {{ $attribute->code == 'name' && ! $product[$attribute->code]  ? 'v-slugify-target=\'url_key\'' : ''  }} @endif />
 
 
 
